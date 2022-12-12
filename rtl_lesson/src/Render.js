f9015fa8a63d620416f891993f353ee0b0be5b4d
seamoooo <hm.seamo@gmail.com>
2022-12-13 00:37:15 +0900

    add jest and react-testing-lib

---
+++ rtl_lesson/src/Render.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Render extends Component {
+  render() {
+    return <div>
+      <h1>react testing library lesson</h1>
+      <input type="text"/>
+      <button>clic1</button>
+      <button>clic4</button>
+      <p>udemy</p>
+      <span>@react</span>
+      </div>
+  }
+}
