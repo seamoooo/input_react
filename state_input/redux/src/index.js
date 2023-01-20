98094ca12fa0ede513040ada588828acd4d3b170
seamoooo <hm.seamo@gmail.com>
2023-01-20 23:01:46 +0900

    add provider and reducer

--- state_input/redux/src/index.js
+++ state_input/redux/src/index.js
@@ -4,46 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import allReducers from './reducers/index.js';
+import { Provider } from "react-redux"
 
-// actions, decrement
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  }; 
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  }; 
-};
-
-// Reducer
-const counterReducer = (state = 0, action) => {
-  switch(action.type){
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-}
-
-// store
-let store = createStore(counterReducer)
-
-// 
-store.subscribe(() => console.log(store.getState()))
-
-//dispatch
-store.dispatch(increment());
-store.dispatch(increment());
-
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
