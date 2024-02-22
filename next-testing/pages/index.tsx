fdb48be21d47da4c3d2ea924f3f3b4848a148927
seamoooo <hm.seamo@gmail.com>
2024-02-22 18:49:20 +0900

    delete

--- next-testing/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout'
-const Home: React.FC = () => {
-  return (
-    <Layout title="Home">
-      <p className="text-4xl">Welcome to Nextjs</p>
-    </Layout>
-  )
-}
-export default Home
